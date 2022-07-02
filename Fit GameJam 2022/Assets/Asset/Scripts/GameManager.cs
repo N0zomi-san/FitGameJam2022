e311e3e7cced6ccf478d096667e524e872a1489a
FriedChickenze <76896720+FriedChickenze@users.noreply.github.com>
2022-07-02 18:09:03 +0700

    Create Paper's prefabs
    +2 Paper's prefabs

---
+++ Fit GameJam 2022/Assets/Asset/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Character Management
+    SpriteRenderer mainCharacter;
+    Animator mainAnimator;
+    #endregion
+
+    #region GamePlay Management
+    Camera maincam = Camera.current;
+    RaycastHit hit;
+    
+    
+
+    #endregion
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Fit GameJam 2022/Assets/Asset/Scripts/ScoreManagement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagement : MonoBehaviour
+{
+    int currentScore;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+    void IncreaseOne(){
+        
+    }
+}
