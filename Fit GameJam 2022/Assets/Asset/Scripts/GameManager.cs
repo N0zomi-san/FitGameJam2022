f5fe41cfc4e21b538d94007e4716fbb73a438366
FriedChickenze <76896720+FriedChickenze@users.noreply.github.com>
2022-07-02 23:09:14 +0700

    Finished Day01

--- Fit GameJam 2022/Assets/Asset/Scripts/GameManager.cs
+++ Fit GameJam 2022/Assets/Asset/Scripts/GameManager.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
     #region Character Management
     SpriteRenderer mainCharacter;
-    Animator mainAnimator;
+    Animator mainCharacterAnimator;
+    [SerializeField] Transform handTransform;
+
     #endregion
 
     #region GamePlay Management
-    Camera maincam = Camera.current;
-    RaycastHit hit;
+    int currentScore; // +1 when player make the right choice
+    int currentPaper; // +1 when player make a choice with a paper
+    int currentNakApproved; // +1 when player approve a Nak's paper
+
     
+    public Camera mainCam;
+    RaycastHit2D hit; // hit for player interaction with mouse
     
+
 
     #endregion
 
-    
+    void AimingRIghtSide(){
+        hit = Physics2D.Raycast(mainCam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider == null){return;}
+        if (hit.collider.tag == "rightSide"){
+            ArmFollow();
+        }
+    }
+
+    void ArmFollow(){
+        Vector3 target = new Vector3(mainCam.ScreenToWorldPoint(Input.mousePosition).x, mainCam.ScreenToWorldPoint(Input.mousePosition).y, -2f);
+            handTransform.transform.position = target;
+    }
+
+    void Approved(){
+
+    }
+
+    void Denied(){
+
+    }
+
+    void SpawnPaper(){
+
+    }
+
     void Start()
     {
-        
+         
     }
 
     
     void Update()
     {
-        
+        AimingRIghtSide();
     }
 }
