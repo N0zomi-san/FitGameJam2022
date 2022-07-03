8ea09dadde56684c156191b12de635de1fc81f76
christang <108562486+christang3@users.noreply.github.com>
2022-07-04 00:47:37 +0700

    Removed Scene

--- Fit GameJam 2022/Assets/Asset/Scripts/Button.cs
+++ Fit GameJam 2022/Assets/Asset/Scripts/Button.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
-
+    public GameObject Canvas;
+    public GameObject Tutorial;
+    public float TutorialTimes = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tutorial.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,10 +22,18 @@ public class Button : MonoBehaviour
 
     public void NextScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
+        StartCoroutine(StartTutorial());
+        Canvas.SetActive(false);
+        Tutorial.SetActive(true);
     }
 
     public void doExitGame() {
         Application.Quit();
+    }
+
+    IEnumerator StartTutorial()
+    {
+        yield return new WaitForSecondsRealtime(TutorialTimes);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
